06cdf4fe7709fea4f4528c0cd5d6ec10681f1f79
David Lindner <dev@davidlindner.me>
2018-03-18 04:01:07 +0100

    remove duplicated ImpactPage

--- ionic_app/FiftyShadesOfGreen/src/pages/impact/impact.module.ts
+++ ionic_app/FiftyShadesOfGreen/src/pages/impact/impact.module.ts
@@ -11,23 +11,3 @@ import { ImpactPage } from './impact';
   ],
 })
 export class ImpactPageModule {}
-
-
-import { Component } from '@angular/core';
-import { ModalController } from 'ionic-angular';
-import { ModalPage } from '../modal/modal';
-
-@Component({
-  selector: 'page-impact',
-  templateUrl: 'impact.html'
-})
-
-export class ImpactPage {
-
-  constructor(public modalCtrl: ModalController) { }
-
-  public openModal() :void {
-    let myModal = this.modalCtrl.create(SampleModalPage);
-    myModal.present();
-  }
-}
